29ffa9a85a5052482e6739fee86f37c9de06bf2d
Khoa Pham <khoa.pham@me.com>
2020-07-14 17:04:10 -0700

    Update layout

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
 import Meta from '../components/meta'
-import Header from '../components/Header'
+import Header from '../components/header'
 
 export default function Layout({ preview, children }) {
   return (
