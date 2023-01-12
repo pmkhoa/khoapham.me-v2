983733b932cbf7222b179ec77c5559de8a12f2a1
Khoa Pham <khoa.pham@me.com>
2023-01-12 09:59:02 +0700

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Index({ allPosts }) {
           <title>Khoa Pham</title>
         </Head>
         <section>
-          <p>Hi there! I'm Khoa Pham, director of technology and senior web application developer in Portland, Oregon.</p>
+          <p>Hi there! I'm Khoa Pham, technologist and senior web developer in Portland, Oregon.</p>
           <p>I'm currently working at <a href="https://romance.website" target="_blank">Romance</a> as Founding Partner and Director of Technology. My main focus is to work with Romance team to deliver products with best user experience  for our clients. I love experimenting with new technologies and focusing on application architecture.</p>
           <p>You can find me on <a target="_blank" href="https://twitter.com/pmkhoa">Twitter</a>, <a target="_blank" href="http://www.linkedin.com/in/pmkhoa">Linkedin</a>, and <a target="_blank" href="https://github.com/pmkhoa">Github</a>.</p>
           <p>In the past few years, I was lucky to have opportunity to work with many talented people to finish projects for clients like: Nike, UberEats, Resku, Joyn, OneSight, SAP, Adidas,  Ratio Coffee, Seastar, Workspot, and others.</p>
